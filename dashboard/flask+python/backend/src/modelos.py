bb7b29e5848e29ea657390078cb79425033a72a5
Manuel Sanchez <mbsanchez@unet.edu.ve>
2020-04-23 20:55:56 +0200

    el modelo seir es agregado y parametrizado en el frontend angular

--- dashboard/flask+python/backend/src/modelos.py
+++ dashboard/flask+python/backend/src/modelos.py
@@ -3,7 +3,8 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
-import mod_sir_covid_19_v1
+import mod_sir_covid_19_v1 as sirv1
+import mod_seir_covid_19_v1 as seir
 
 def create_app():
     app = Flask(__name__)
@@ -28,7 +29,7 @@ def create_app():
 
         y0 = r0
 
-        return mod_sir_covid_19_v1.rk4(mod_sir_covid_19_v1.f, mod_sir_covid_19_v1.g, mod_sir_covid_19_v1.h, x0, y0, z0, t0, tf, betamn, betamx, gamma, n, hh)
+        return sirv1.rk4(sirv1.f, sirv1.g, sirv1.h, x0, y0, z0, t0, tf, betamn, betamx, gamma, n, hh)
 
     @app.route('/sir02')
     def sir02():
@@ -36,6 +37,26 @@ def create_app():
 
     @app.route('/seir01')
     def seir01():
-        return 'Hello, SEIR Analitico 1!'
+        r0 = request.args.get('r0', default = 0, type = float)
+        x0 = request.args.get('x0', default = 0.1, type = float)
+        w0 = request.args.get('w0', default = -1, type = float)
+        z0 = request.args.get('z0', default = 0, type = float)
+        t0 = request.args.get('t0', default = 0, type = int)
+        tf = request.args.get('tf', default = 15, type = int)
+        betamn = request.args.get('betamn', default = 9, type = float)
+        betamx = request.args.get('betamx', default = 10, type = float)
+        gamma = request.args.get('gamma', default = 1, type = float)
+        alfa = request.args.get('alfa', default = 2, type = float)
+        hh = request.args.get('hh', default = 0.01, type = float)
+
+        n = int((tf - t0) / hh)
+
+        if w0 < 0:
+            w0 = 1 - r0
+
+        y0 = r0
+
+        return seir.rk4(seir.f, seir.g, seir.h, seir.i, w0, x0, y0, z0, t0, tf, alfa, betamn, betamx, gamma, n, hh)
+
 
     return app
--- dashboard/flask+python/frontend/src/app/app.component.ts
+++ dashboard/flask+python/frontend/src/app/app.component.ts
@@ -2,23 +2,9 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
-/*export interface SIR01Data {
-  tiempo;
-  susceptibles;
-  infectados;
-  removidos;
-}*/
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-
-export class AppComponent {
-  title = 'frontend';
-  modSIR01Data: any;
-  modSIR01DataInf: any;
+export class SIR01Data {
+  modData: any;
+  modDataInf: any;
   simdays: number; // numero de días
   tstart: number;  // tiempo inicial
   r0: number;      // cantidad inicial de contagiados max 1
@@ -29,7 +15,7 @@ export class AppComponent {
   gamma: number;   // velocidad de recuperación
   hh: number;      // paso del modelo
 
-  constructor(private httpClient: HttpClient) {
+  constructor(){
     this.tstart=0;
     this.simdays=15;
     this.r0=0.0000045;
@@ -40,21 +26,55 @@ export class AppComponent {
     this.gamma=1;
     this.hh=0.01;
   }
+}
+
+export class SEIR01Data extends SIR01Data {
+    w0: number;      // suceptibles in the SEIR model
+    alfa: number;    // tasa de exposicion
+
+  constructor(){
+    super();
+    this.tstart=0;
+    this.simdays=15;
+    this.r0=0;
+    this.x0=0.1;
+    this.z0=0;
+    this.w0=-1;
+    this.betamn=9;
+    this.betamx=10;
+    this.gamma=1;
+    this.alfa=2;
+    this.hh=0.01;
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'frontend';
+  sir01Data: SIR01Data;
+  seir01Data: SEIR01Data;
+  
+
+  constructor(private httpClient: HttpClient) {
+    this.sir01Data = new SIR01Data;
+    this.seir01Data = new SEIR01Data;    
+  }
 
   ngOnInit() {
     this.sir001();
+    this.seir001();
   }
 
   sir001() {
-    /*this.httpClient.get<any>('http://127.0.0.1:5000/sir01')
-      .toPromise()
-      .then(res => <SIR01Data[]>res.data)
-      .then(data => this.modSIR01Data = data);*/
-
-    let tf=this.tstart+this.simdays;
-    this.httpClient.get<any>(`http://127.0.0.1:5000/sir01?t0=${this.tstart}&tf=${tf}&r0=${this.r0}&x0=${this.x0}&z0=${this.z0}&betamn=${this.betamn}&betamx=${this.betamx}&gamma=${this.gamma}&hh=${this.hh}`)
+    let tf=this.sir01Data.tstart+this.sir01Data.simdays;
+    this.httpClient.get<any>(`http://127.0.0.1:5000/sir01?t0=${this.sir01Data.tstart}&tf=${tf}&r0=${this.sir01Data.r0}&x0=${this.sir01Data.x0}&z0=${this.sir01Data.z0}&betamn=${this.sir01Data.betamn}&betamx=${this.sir01Data.betamx}&gamma=${this.sir01Data.gamma}&hh=${this.sir01Data.hh}`)
       .subscribe((data: JSON) => {
-         this.modSIR01Data = {
+         this.sir01Data.modData = {
             labels: data['data']['data'][0],
             datasets: [
                 {
@@ -72,7 +92,7 @@ export class AppComponent {
             ]
         };
 
-        this.modSIR01DataInf = {
+        this.sir01Data.modDataInf = {
             labels: data['data']['data'][0],
             datasets: [
                 {
@@ -87,59 +107,47 @@ export class AppComponent {
   }
 
   sir002() {
-    /*this.httpClient.get('http://localhost:5000/sir02').subscribe(data => {
-      this.modSIR02Data = data as JSON;
-      console.log(this.modSIR02Data);
-    })*/
+  }
 
-        /*this.httpClient.get<any>('http://127.0.0.1:5000/sir01')
-          .toPromise()
-          .then(res => <SIR01Data[]>res.data)
-          .then(data => this.modSIR01Data = data);*/
-
-   /* this.httpClient.get<any>('http://127.0.0.1:5000/sir01')
-      .subscribe((data : any[]) => {
-        /*this.data.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
-        this.data.datasets[0].data = [65, 59, 80, 81, 56, 55, 40]
-        this.data.datasets[0].label = 'Infectados';
-	this.data.datasets[0].fill = false;
-        this.data.datasets[0].borderColor= '#4bc0c0';*/
-
-        /*this.data = {
-            labels: data.data[0],
+  seir001() {
+    let tf=this.seir01Data.tstart+this.seir01Data.simdays;
+    this.httpClient.get<any>(`http://127.0.0.1:5000/seir01?t0=${this.seir01Data.tstart}&tf=${tf}&r0=${this.seir01Data.r0}&x0=${this.seir01Data.x0}&w0=${this.seir01Data.w0}&z0=${this.seir01Data.z0}&betamn=${this.seir01Data.betamn}&betamx=${this.seir01Data.betamx}&gamma=${this.seir01Data.gamma}&alfa=${this.seir01Data.alfa}&hh=${this.seir01Data.hh}`)
+      .subscribe((data: JSON) => {
+         this.seir01Data.modData = {
+            labels: data['data']['data'][0],
             datasets: [
                 {
-                    label: data.columns[1],
-                    data: data.data[1],
+                    label: data['data']['index'][1],
+                    data: data['data']['data'][1],
                     fill: false,
                     borderColor: '#4bc0c0'
                 },
                 {
-                    label: data.columns[2],
-                    data: data.data[2],
+                    label: data['data']['index'][4],
+                    data: data['data']['data'][4],
+                    fill: false,
+                    borderColor: '#ff0000'
+                }
+            ]
+        };
+
+        this.seir01Data.modDataInf = {
+            labels: data['data']['data'][0],
+            datasets: [
+                {
+                    label: data['data']['index'][2],
+                    data: data['data']['data'][2],
                     fill: false,
                     borderColor: '#565656'
+                },
+                {
+                    label: data['data']['index'][3],
+                    data: data['data']['data'][3],
+                    fill: false,
+                    borderColor: '#00ff00'
                 }
             ]
         }
-      });*/
-
-      /*this.data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    borderColor: '#4bc0c0'
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    borderColor: '#565656'
-                }
-            ]
-        }*/
+      });
   }
 }
