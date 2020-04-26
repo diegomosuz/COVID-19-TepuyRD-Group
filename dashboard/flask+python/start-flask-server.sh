ed08596eb6fbc08cf584c09da271d6ad2f2ae70d
Manuel Sanchez <mbsanchez@unet.edu.ve>
2020-04-26 11:31:46 +0200

    se usa variable --prod para iniciar el servidor flask en modo produccion

--- dashboard/flask+python/start-flask-server.sh
+++ dashboard/flask+python/start-flask-server.sh
@@ -4,6 +4,10 @@
 pipenv --three && pipenv sync
 #run the server
 export FLASK_APP=./backend/src/modelos.py
-export FLASK_ENV=development
+if [ "$1" != "--prod" ]; then
+  export FLASK_ENV=development
+else
+  export FLASK_ENV=production
+fi
 source $(pipenv --venv)/bin/activate
 flask run -h 0.0.0.0
