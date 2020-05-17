32a52dc1c6adea9a1504dcaf9d78a2b8ad006ff2
Manuel Sanchez <mbsanchez@unet.edu.ve>
2020-05-17 19:02:48 +0200

    se agregaron comandos que hacian falta para inicializar el servidor de angular

--- dashboard/flask+python/angular-installer.sh
+++ dashboard/flask+python/angular-installer.sh
@@ -3,4 +3,9 @@
 
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash - && sudo apt install nodejs
 sudo npm install -g @angular/cli@9
+cd frontend && sudo npm install && cd -
 
+#For centos (Redhat based)
+#curl -sL https://rmp.nodesource.com/setup_13.x | sudo -E bash - && sudo yum install nodejs
+#sudo npm install -g @angular/cli@9
+#cd frontend && sudo npm install && cd -
